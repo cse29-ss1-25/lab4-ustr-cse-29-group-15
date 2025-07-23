6ef0c744bf4fed0be086c50a432e2c0fb91310dd
Yang <xiy070@ieng6-203.ucsd.edu>
2025-07-23 11:12:31 -0700

    Cindy: substring + concat

--- ustr.c
+++ ustr.c
@@ -37,8 +37,25 @@ and ending at index end (exclusive).
 Returns an empty string on invalid range.
 */
 UStr substring(UStr s, int32_t start, int32_t end) {
-	// TODO: implement this
+    if(start <0 || start >= end || end> s.codepoints){
+    return new_ustr("");
+    }
 
+    int start_byte = bi_of_cpi(s.contents, start);
+    int end_byte = bi_of_cpi(s.contents, end);
+
+    char* new_contents  = malloc(end_byte- start_byte + 1);
+    if(! new_contents){
+	 return new_ustr("");
+    }
+
+    memcpy(new_contents, s.contents + start_byte, end_byte- start_byte);    
+    new_contents[end_byte- start_byte] = '\0';
+
+    UStr result = new_ustr(new_contents);
+    free(new_contents);
+
+    return result;
 }
 
 /*
@@ -46,8 +63,23 @@ Given 2 strings s1 and s2, returns a string that is the result of
 concatenating s1 and s2. 
 */
 UStr concat(UStr s1, UStr s2) {
-	// TODO: implement this
+    char* new_contents = malloc(s1.bytes + s2.bytes + 1);
+    if(!new_contents){
+	    return new_ustr("");
+    }
 
+    memcpy(new_contents, s1.contents, s1.bytes);
+    memcpy(new_contents + s1.bytes, s2.contents, s2.bytes);
+    new_contents[s1.bytes + s2.bytes] = '\0';
+
+    UStr result;
+
+    result.contents = new_contents;
+    result.codepoints = s1.codepoints + s2.codepoints;
+    result.bytes =  s1.bytes + s2.bytes;
+    result.is_ascii = s1.is_ascii && s2.is_ascii;
+
+    return result;
 }
 
 /*
